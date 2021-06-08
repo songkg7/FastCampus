7319c81dee19695329b7e4ee58dfa77d3a3e7256
Haril Song <songkg7@gmail.com>
2021-06-08 15:16:49 +0900

    Algorithm 3 - InsertionSort

---
+++ src/basic/study25/InsertionSort.java
@@ -0,0 +1,42 @@
+package basic.study25;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr, int count) {
+
+        int temp;
+
+        for (int i = 1; i < count; i++) {
+            temp = arr[i];
+            int j = i;
+            while ((j > 0) && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j -= 1;
+            }
+
+            arr[j] = temp;
+
+            System.out.println("반복 - " + i);
+            printSort(arr);
+        }
+
+    }
+
+    public static void printSort(int[] values) {
+
+        for (int value : values) {
+            System.out.println(value + "\t");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {80, 50, 70, 10, 60, 20, 40, 30};
+
+        insertionSort(arr, 8);
+
+    }
+
+}
